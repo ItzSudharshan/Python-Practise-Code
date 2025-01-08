97ad38ce8be627036d22caeb910af4e5509a182a
ItzSudharshan <Nayaksudharshan19@gmail.com>
2025-01-08 19:51:03 +0530

    Updated DataType Files

---
+++ Jan7VariableDataTypes/DataTypes.py
@@ -0,0 +1,60 @@
+''''
+data Types will help us for defining the type of values a variable and hold and what are the different operations that can be performed on the data that is Stored
+
+
+
+There are 9 Different DataTypes in Python 
+1) Integer(int)
+2) Float
+3) Boolean (bool)
+4) String (str)
+5) Sets (set)
+6) Complex Number (complex)
+7) List 
+8) tuple 
+9) Distinctonaries(dict)
+
+
+
+
+'''
+
+#Integer
+print("1.INTEGER")
+number_of_apples = 5
+print(type(number_of_apples))
+print('------------------------------')
+print('2.FLOAT')
+weight = 2.5
+print(type(weight))
+print('------------------------------')
+print('3.STRING')
+message = 'Hello World!'
+print(type(message))
+print('------------------------------')
+print('4.Boolean')
+is_active = 'True'
+print(type(is_active))
+print('------------------------------')
+print("5.SET")
+unique_number = {1,2,3,4,5}
+print(type(unique_number))
+print('------------------------------')
+print('6.COMPLEX NUMBER')
+complex_number = 3 + 2j
+print(type(complex_number))
+print('------------------------------')
+print("7.LIST")
+shopping_list = ["Apple","Banana","Orange"]
+print(type(shopping_list))
+print('------------------------------')
+print("8.TUPLE")
+albun = ("Song1","Song2","Song3")
+print(type(albun))
+print('------------------------------')
+print("9.DISCTIONARIES")
+phone_book = {
+    "Alice":"555-1234",
+    "Bob":"555-5678"
+}
+print(type(phone_book))
