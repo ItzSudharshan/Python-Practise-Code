64669508595094f08dfd6fa1f17a0470fa60c9b1
ItzSudharshan <Nayaksudharshan19@gmail.com>
2025-01-08 20:51:06 +0530

    Updated List.py file

--- Jan8/List.py
+++ Jan8/List.py
@@ -24,3 +24,21 @@ print("Slicing a List")
 sublist = my_list[1:3]  #last element isnt Included
 print(sublist)
 print('------------------------')
+
+'''
+Difference Between List and Tupple
+
+----------------------LIST--------------------
+1) List is Represented by []
+2) List is Mutable . Once List is Created we can do some changes in it 
+3) If the content is not fixed and keeps on changing we need to make use of list 
+4) List Objects cannot be used as keys for dictionaries because it requires values to be immutable and hashable
+
+
+----------------TUPLE-----------------------------
+1) Tuple is Represented by ()
+2) Tuple is Immutable . Once a tuple is created we cannot do chnages to it 
+4) if content is fixed and doesnt chnage tuple is to be used 
+5) Tuple objects can be used as keys for dictionaries because its values are immutable and hashable
+
+'''
