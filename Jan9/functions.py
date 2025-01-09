de481abe1892b5789bfc90b738aae710fc3d8ab9
ItzSudharshan <Nayaksudharshan19@gmail.com>
2025-01-09 22:30:53 +0530

    Updates functions file

--- Jan9/functions.py
+++ Jan9/functions.py
@@ -27,3 +27,21 @@ add()
 sub(19,5)
 print(mul())
 print(div(200,10))
+
+
+'''
+Interviewer: What are functions in Python, and how do they simplify coding?
+
+Ideal Answer: Functions in Python are like reusable pieces of code that you write once and can use multiple times. They help in automating repetitive tasks, making the code cleaner, easier to read, and more efficient.
+-------------------------------------------------------------------------------------------------------------------------------------------
+
+Interviewer: What are the four different types of functions discussed in the session, and what unique purpose does each serve?
+
+Ideal Answer: The four different types of functions discussed are:
+
+No arguments + No return value: These functions don't require any input and don't return any output. They simply perform a task, like playing a song to hype people up.
+No arguments + Return value: These functions don't require any input but return a result. An example is a function that tells you the current weather.
+Arguments + No return value: These functions require input to perform a task but don't return any output. An example is a function that personalizes a greeting message based on the name and activity provided.
+Arguments + Return value: These functions require input and return a result. An example is a function that calculates the area of a rectangle based on the provided length and width
+
+'''
