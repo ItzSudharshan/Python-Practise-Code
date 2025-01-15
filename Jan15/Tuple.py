5036ee81dad52a066319d50810e4994136ddfa0a
ItzSudharshan <Nayaksudharshan19@gmail.com>
2025-01-15 23:21:35 +0530

    Updated Tuple.py file

--- Jan15/Tuple.py
+++ Jan15/Tuple.py
@@ -3,13 +3,21 @@ tuple?
 Tuples are ordered, immutable collections of items. Like lists, they can contain 
 items of different data types, but their items are enclosed in parentheses (( ))
 
-
+A tuple is a collection of items that is ordered and unchangeable. Unlike lists, which allow you to modify their elements, tuples lock their values in place after creation. This makes tuples ideal when you need data to remain constant and unchangeable.
 
 
 In tuple we can store homogenous as well as Heterogenous Data.
 In Tuples we can Store Duplicates
 Tuples are Ordered Collection of Data 
 Tuple are Immutable : Once we declare the tuple we cannot modify it 
+Example : 
+days_of_week = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
+print(days_of_week)
+
+Output:
+('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
+
+
 
 '''
 #Tuples wont allow u to add the element , remove the element or even replace the element . Deleting the Whole Object is Allowed
